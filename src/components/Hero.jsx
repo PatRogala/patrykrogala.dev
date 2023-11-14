8bc0ab2427d4c32d4ca79221498cd24794adb6b4
Patryk Rogala <rogala.patryk@pm.me>
2023-11-14 19:59:59 +0100

    Change Color Scheme

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl max-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-5 h-5 rounded-full bg-red-500" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Patryk</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-red-500">Patryk</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop Ruby On Rails <br className="sm:block hidden" /> applications.</p>
         </div>
       </div>
