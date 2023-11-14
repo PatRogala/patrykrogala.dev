117129f72d31a46014f3f49ef349825022285b50
Patryk Rogala <rogala.patryk@pm.me>
2023-11-15 00:23:40 +0100

    change light color to red

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,9 +8,9 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.2} groundColor="black" />
-      <pointLight intensity={1} />
-      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+      <hemisphereLight intensity={1.2} groundColor="red" color={0xff0000} />
+      <pointLight intensity={1} color={0xff0000} />
+      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} color={0xff0000} />
       <primitive object={computer.scene} scale={ isMobile ? 0.7 : 0.75} position={ isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
     </mesh>
   )
