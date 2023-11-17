b9b840ee543989539ba370f78f2d96835f8f54af
Patryk Rogala <rogala.patryk@pm.me>
2023-11-17 11:19:51 +0100

    Stars

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -14,11 +14,13 @@ const Earth = () => {
 
 const EarthCanvas = () => {
   return (
-    <Canvas shadows frameLoop="demand" gl={{ preserveDrawingBuffer: true }} camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}>
+    <Canvas shadows frameloop="demand" gl={{ preserveDrawingBuffer: true }} camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Earth />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   )
 }
