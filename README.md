f931762ed3b354027ba6ad451521194156467528
Patryk Rogala <rogala.patryk@pm.me>
2023-11-17 12:09:15 +0100

    Delete unused assets and update README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Tech Stack
+# patrykrogala.dev
 
-React
-Tailwind
+![App Screenshot](screenshot.png)
+
+A personalized website showcasing my skills in Ruby on Rails.
+
+This project serves as a platform to highlight my proficiency in Ruby on Rails development. It provides a comprehensive overview of my expertise, projects, and achievements in the field. Whether you're a potential employer, collaborator, or just curious about my work, this portfolio is designed to provide insight into my skills and experiences.
+
+[![](https://img.shields.io/badge/-React-332B40?style=for-the-badge&logo=react)]()
+[![](https://img.shields.io/badge/-Tailwind-332B40?style=for-the-badge&logo=tailwindcss)]()
+[![](https://img.shields.io/badge/-Three.js-332B40?style=for-the-badge&logo=three.js)]()
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,25 +1,15 @@
 import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
-import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 import ruby from "./tech/ruby.png";
 import rails from "./tech/rails.png";
 import psql from "./tech/psql.png";
@@ -28,45 +18,23 @@ import bitbucket from "./tech/bitbucket.png";
 import postman from "./tech/postman.png";
 import vscode from "./tech/vscode.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
 import koleo from "./company/koleo.png";
-import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
 import portfolio from "./portfolio.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 export {
   logo,
   backend,
   creator,
-  mobile,
   web,
   github,
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
   koleo,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   ruby,
   rails,
   psql,
