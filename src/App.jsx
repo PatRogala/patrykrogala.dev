3cc8d1b818d69280878aabeb9e8e8480b1a000fa
Patryk Rogala <patryk@astarium.pl>
2024-12-25 21:19:06 +0100

    ✨ (setup): add Tailwind CSS, PostCSS, and React Three Fiber for enhanced UI and 3D rendering capabilities 📝 (App.jsx, RubyCanvas.jsx): implement a new RubyCanvas component and update App to include it
    New configuration files for Tailwind CSS and PostCSS are added to enable utility-first styling. The application now utilizes React Three Fiber and Drei for 3D rendering, showcasing a low-poly ruby model. The App component is updated to include a visually appealing layout with a centered canvas and overlay, enhancing the user interface and experience. This refactor aims to modernize the application and improve its visual appeal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import RubyCanvas from './components/RubyCanvas'
+
 function App() {
   return (
     <>
-      Nello World
+      <div className="flex items-center justify-center absolute inset-0 pointer-events-none">
+        <div id="text-behind">RUBY<br />ON RAILS<br />DEVELOPER</div>
+        <div id="text-behind-blur">RUBY<br />ON RAILS<br />DEVELOPER</div>
+        <div id="text-front">RUBY<br />ON RAILS<br />DEVELOPER</div>
+      </div>
+
+      <div className="canvas-container" style={{width: "100vw", height: "100vh"}}>
+        <RubyCanvas />
+      </div>
+
+
+      <div className="overlay">
+        <header>
+          <p>@patryk_rogala</p>
+        </header>
+        <footer>
+          <h2>Patryk Rogala</h2>
+          <ul>
+            <li><a href="https://www.instagram.com/patrykrogala.dev/" target="_blank"><img src="./icons/instagram.svg" alt="ig-icon" /></a></li>
+            <li><a href="https://www.linkedin.com/in/patrogala/" target="_blank"><img src="./icons/linkedin.svg" alt="ig-icon" /></a></li>
+            <li><a href="https://www.youtube.com/@patryk_rogala" target="_blank"><img src="./icons/youtube.svg" alt="ig-icon" /></a></li>
+          </ul>
+        </footer>
+      </div>
     </>
   )
 }
