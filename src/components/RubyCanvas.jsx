dd250c543942c78c8b6d3753823143fa40f25ad7
Patryk Rogala <patryk@astarium.pl>
2024-12-25 22:26:03 +0100

    ✨ (RubyCanvas): enhance Ruby component with material properties and environment 🔧 (scene.gltf): update GLTF model structure and properties for better rendering
    The Ruby component now applies transparency and material properties to the mesh, enhancing its visual appearance. Additionally, the environment preset is set to "city" for improved background rendering. The GLTF model has been updated to reflect changes in structure and properties, ensuring compatibility and better performance in rendering. These changes aim to provide a more visually appealing and flexible 3D representation of the Ruby object.

--- src/components/RubyCanvas.jsx
+++ src/components/RubyCanvas.jsx
@@ -1,17 +1,31 @@
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { OrbitControls, Preload, useGLTF, Environment } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Suspense, useEffect, useState } from "react"
 
 const Ruby = ({ isMobile }) => {
   const ruby = useGLTF('/src/objects/low_poly_ruby/scene.gltf')
 
+  useEffect(() => {
+    ruby.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.material.transparent = true;
+        child.material.opacity = 0.8;
+        child.material.roughness = 0.7;
+        child.material.metalness = 1;
+      }
+    });
+  }, [ruby]);
+
   return (
-    <>
-      <mesh>
-        <pointLight intensity={200000} position={[0, 2, -10]} />
-        <pointLight intensity={200000} position={[0, -2, 10]} />
-        <primitive object={ruby.scene} scale={ isMobile ? 0.5 : 1.4 } position={ isMobile ? [0.5, -0.5, 0] : [1, -2, 0]} rotation={[0, 0.8, 1]} />
-      </mesh>
+    <>        
+        <mesh>
+            <primitive 
+            object={ruby.scene} 
+            scale={isMobile ? 0.7 : 1.8} 
+            position={[0, 0, 0]}
+            rotation={[0, 0.1, 0.4]} 
+            />
+        </mesh>
     </>
   )
 }
@@ -36,8 +50,12 @@ const RubyCanvas = () => {
   }, [])
 
   return (
-    <Canvas camera={{ position: [0, 0, 8], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas 
+      camera={{ position: [0, 0, 5], fov: 45 }} 
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={null}>
+        <Environment preset="city" />
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} autoRotate />
         <Ruby isMobile={isMobile} />
       </Suspense>
